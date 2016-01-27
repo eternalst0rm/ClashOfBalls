9b833622fb058eefb9213ca1639f0ab865a55ad4
BlackClaw <blackclaw343@gmail.com>
2016-01-27 22:38:32 +0100

    Update 1/27/2016: Project Update
    - small fixes on project

--- Source/ClashOfBalls/ClashOfBalls.Build.cs
+++ Source/ClashOfBalls/ClashOfBalls.Build.cs
@@ -7,5 +7,15 @@ public class ClashOfBalls : ModuleRules
     public ClashOfBalls(TargetInfo Target)
 	{
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
-	}
+
+        //Uncomment if you are using online features
+        PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64))
+        {
+            if (UEBuildConfiguration.bCompileSteamOSS == true)
+            {
+                DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
+            }
+        }
+    }
 }
